006238e21a0fdb299b4d864811feadd367bf7ea2
LuciaBSep <luciabsep18@gmail.com>
2021-03-07 12:47:21 -0300

    change loading

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -1,23 +1,26 @@
 import * as React from "react"
 import {motion} from "framer-motion"
+import {Center} from "@chakra-ui/react"
 
 const Loading: React.FC = () => {
   return (
-    <motion.div
-      style={{backgroundColor: "blue", width: "50px", height: "50px"}}
-      animate={{
-        scale: [0.5, 2, 1],
-        rotate: [0, 270, 0],
-        borderRadius: ["20%", "50%", "20%"],
-      }}
-      transition={{
-        duration: 1,
-        ease: "easeInOut",
-        times: [0, 0.5, 1],
-        loop: Infinity,
-        repeatDelay: 1,
-      }}
-    />
+    <Center h="700px" m="auto" width="480px" bg="brand.500">
+      <motion.div
+        style={{backgroundColor: "white", width: "50px", height: "50px"}}
+        animate={{
+          scale: [0.5, 2, 1],
+          rotate: [0, 270, 0],
+          borderRadius: ["20%", "50%", "20%"],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+          times: [0, 0.5, 1],
+          loop: Infinity,
+          repeatDelay: 1,
+        }}
+      />
+    </Center>
   )
 }
 
