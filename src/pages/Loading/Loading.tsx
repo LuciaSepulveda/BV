9670488bf3f136e938e80ed4db7c4b42b6e3997f
LuciaBSep <luciabsep18@gmail.com>
2021-03-07 15:14:19 -0300

    final

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -1,25 +1,10 @@
 import * as React from "react"
-import {motion} from "framer-motion"
-import {Center} from "@chakra-ui/react"
+import {Center, Spinner} from "@chakra-ui/react"
 
 const Loading: React.FC = () => {
   return (
     <Center bg="brand.500" h="700px" m="auto" width="480px">
-      <motion.div
-        animate={{
-          scale: [0.5, 2, 1],
-          rotate: [0, 270, 0],
-          borderRadius: ["20%", "50%", "20%"],
-        }}
-        style={{backgroundColor: "white", width: "50px", height: "50px"}}
-        transition={{
-          duration: 0.5,
-          ease: "easeInOut",
-          times: [0, 0.5, 1],
-          loop: Infinity,
-          repeatDelay: 1,
-        }}
-      />
+      <Spinner size="xl" speed="0.6s" color="pink" thickness="4px" />
     </Center>
   )
 }
