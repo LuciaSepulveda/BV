50c97e8d920524d3c5f8e28ec98a81f1a60ebade
LuciaBSep <luciabsep18@gmail.com>
2021-03-07 12:36:25 -0300

    add Chakra to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
+import {ChakraProvider, extendTheme} from "@chakra-ui/react"
+
 import App from "./App/App"
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+    600: "#556bf4",
+    500: "#6476e0",
+  },
+}
+
+const theme = extendTheme({colors})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 )
