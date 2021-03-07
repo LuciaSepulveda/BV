2d373237ee2f953b490bdf8694458c9f2b1d50cf
LuciaBSep <luciabsep18@gmail.com>
2021-03-07 12:36:47 -0300

    create api

---
+++ src/api/api.ts
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+import {Question} from "../types/Question"
+
+export default {
+  list: (n: number): Promise<Question[]> =>
+    fetch(`https://opentdb.com/api.php?amount=10&category=${n}`)
+      .then((res) => res.json())
+      .then((res) => res.results),
+}
